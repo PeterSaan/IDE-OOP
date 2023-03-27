b276bb42819e95e1a529db1f3d961aecf706793e
PeterSaan <peter.saan@tptlive.ee>
2023-03-27 22:56:37 +0300

    15.03 tunnikas

--- ExpenseIt/ExpenseIt/ExpenseItHome.xaml.cs
+++ ExpenseIt/ExpenseIt/ExpenseItHome.xaml.cs
@@ -20,9 +20,17 @@ namespace ExpenseIt
     /// </summary>
     public partial class ExpenseItHome : Page
     {
+
         public ExpenseItHome()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
         }
     }
 }
