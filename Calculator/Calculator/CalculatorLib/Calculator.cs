9098cbd7a41770380032ccdb417d2b764b694428
PeterSaan <peter.saan@tptlive.ee>
2023-04-19 14:07:59 +0300

    Kalkukas

---
+++ Calculator/Calculator/CalculatorLib/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        private float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        private float Sub(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        private float Mult(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        private float Div(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+
+        // Võtab vastu nupuvajutusi
+        public void KeyPress(char key)
+        {
+
+        }
+
+        // Tagastab tulemuse
+        public decimal? Result
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
