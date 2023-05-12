5676ab585fdba4c0d9b19c2dc664f828856694ba
PeterSaan <peter.saan@tptlive.ee>
2023-05-12 11:35:59 +0300

    12.05 OOP

---
+++ ObjectToObjectMapper/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+// https://www.twilio.com/blog/building-blazing-fast-object-mapper-c-sharp-net-core
+Console.WriteLine("Hello, World!");
