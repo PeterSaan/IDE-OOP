56d436d06ca14c3d420a936cc694926a48ea5291
PeterSaan <peter.saan@tptlive.ee>
2023-05-25 18:39:38 +0300

    Angular and .NET API

---
+++ AngularDemo/AngularDemo/Startup.cs
@@ -0,0 +1,35 @@
+﻿using AngularDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Startup
+{
+    public IConfiguration configRoot { get; }
+    public Startup(IConfiguration configuration)
+    {
+        configRoot = configuration;
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        string connectionString = Configuration.GetConnectionString("default");
+        services.AddDbContext<AppDBContext>(c => c.UseSqlServer(connectionString));
+    }
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.Run();
+    }
+}
